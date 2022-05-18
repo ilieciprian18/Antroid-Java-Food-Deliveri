11b2aeb8b0e0d385f13063f95a0e68fde1dda5a7
unknown <ilieciprian18@yahoo.com>
2022-05-18 12:06:13 +0300

    da

---
+++ app/src/main/java/com/example/myapplication/KFC.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KFC extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    List<String> foodList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        foodList = new ArrayList<>();
+        foodList.add("Meniu 5 Crispy Strips Mare;28");
+        foodList.add("Meniu 5 Crispy Strips Mediu;24");
+        foodList.add("Meniu 8 Crispy Strips Mare;38");
+        foodList.add("Meniu 8 Crispy Strips Mediu;34");
+
+        foodList.add("SEND ORDER;0");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(foodList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                recyclerAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
